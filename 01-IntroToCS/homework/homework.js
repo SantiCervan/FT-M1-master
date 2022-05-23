6d7878df31db217b9adb69ddff9ecf7975dae3f5
unknown <santi.cervan.12@gmail.com>
2022-05-23 17:04:14 -0300

    finalizada homework 1

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -1,17 +1,29 @@
 'use strict'
 
 function BinarioADecimal(num) {
-  // tu codigo aca
-
+    // tu codigo aca
+    let nuevoArray = num.split('').reverse();
+    let suma = 0
+    for (let i = 0; i < nuevoArray.length; i++) {
+        suma = suma + nuevoArray[i] * (2 ** i)
+    }
+    return suma
 }
+
 
 function DecimalABinario(num) {
-  // tu codigo aca
-
+    // tu codigo aca
+    let nuevoArray = []
+    while (num != 0) {
+        let div = Math.floor(num / 2)
+        let residuo = parseInt(num % 2)
+        num = div
+        nuevoArray.push(residuo)
+    }
+    return nuevoArray.reverse().join('')
 }
 
-
 module.exports = {
-  BinarioADecimal,
-  DecimalABinario,
+    BinarioADecimal,
+    DecimalABinario,
 }
